bdc3c0a3bdc79161aeda3971eb56275d4b80939c
David Campbell III <lapidistcubed@gmail.com>
2024-05-15 00:09:54 -0500

    Added phone checking and peeking

---
+++ Assets/Scripts/BG/Phone.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using MyBox;
+using UnityEngine;
+
+public enum PhoneState
+{
+    AWAY, PEEKING, CHECKING
+}
+
+public class Phone : MonoBehaviour
+{
+    [SerializeField, MustBeAssigned]
+    private Transform awayPosition;
+    [SerializeField, MustBeAssigned]
+    private Transform peekPosition;
+    [SerializeField, PositiveValueOnly]
+    private float timeToPeek = 1f;
+    [SerializeField, MustBeAssigned]
+    private Transform checkPosition;
+    [SerializeField, PositiveValueOnly]
+    private float timeToCheck = 3f;
+    
+    public PhoneState state { get; private set; } = PhoneState.AWAY;
+    
+    private Transform thisTransform;
+    
+    private void Start()
+    {
+        thisTransform = this.transform;
+        state = PhoneState.AWAY;
+        
+        thisTransform.localPosition = awayPosition.localPosition;
+        thisTransform.localRotation = awayPosition.localRotation;
+    }
+    
+    public void Check()
+    {
+        float time = state == PhoneState.AWAY ? timeToCheck : timeToCheck - timeToPeek;
+        thisTransform.DOKill();
+        thisTransform.DOLocalMove(checkPosition.localPosition, time);
+        thisTransform.DOLocalRotate(checkPosition.localEulerAngles, time);
+        state = PhoneState.CHECKING;
+    }
+    
+    public void Peek()
+    {
+        thisTransform.DOKill();
+        thisTransform.DOLocalMove(peekPosition.localPosition, timeToPeek);
+        thisTransform.DOLocalRotate(peekPosition.localEulerAngles, timeToPeek);
+        state = PhoneState.PEEKING;
+    }
+    
+    public void PutAway()
+    {
+        float time = state == PhoneState.CHECKING ? timeToCheck : timeToPeek;
+        thisTransform.DOKill();
+        thisTransform.DOLocalMove(awayPosition.localPosition, time);
+        thisTransform.DOLocalRotate(awayPosition.localEulerAngles, time);
+        state = PhoneState.AWAY;
+    }
+}
--- Assets/Scripts/Player/PlayerActionManager.cs
+++ Assets/Scripts/Player/PlayerActionManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using MyBox;
@@ -9,10 +10,38 @@ public class PlayerActionManager : MonoBehaviour
     private PlayerMovement movement;
     [SerializeField, MustBeAssigned]
     private Interact interact;
+    [SerializeField, MustBeAssigned]
+    private Phone phone;
     
     public void ToggleInActivity(bool isActive)
     {
         movement.enabled = !isActive;
         interact.enabled = !isActive;
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("PeekBG"))
+        {
+            if(phone.state == PhoneState.AWAY)
+            {
+                phone.Peek();
+            }
+            else if(phone.state == PhoneState.PEEKING)
+            {
+                phone.PutAway();
+            }
+        }
+        else if(Input.GetButtonDown("CheckBG"))
+        {
+            if(phone.state == PhoneState.AWAY || phone.state == PhoneState.PEEKING)
+            {
+                phone.Check();
+            }
+            else if(phone.state == PhoneState.CHECKING)
+            {
+                phone.PutAway();
+            }
+        }
     }
 }
