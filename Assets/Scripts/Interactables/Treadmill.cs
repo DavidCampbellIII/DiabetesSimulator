c2bdebf63b5fa221093b33cd06c9624e74fa3df7
David Campbell III <lapidistcubed@gmail.com>
2024-05-16 01:35:43 -0500

    Added timer to treadmill

--- Assets/Scripts/Interactables/Treadmill.cs
+++ Assets/Scripts/Interactables/Treadmill.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using MyBox;
+using TMPro;
 using UnityEngine;
 
 public class Treadmill : MonoBehaviour, IInteractable
@@ -14,6 +15,8 @@ public class Treadmill : MonoBehaviour, IInteractable
     private Transform playerWalkingPoint;
     [SerializeField, MustBeAssigned]
     private Transform dismountPoint;
+    [SerializeField, MustBeAssigned]
+    private TextMeshPro timerText;
     
     [SerializeField, PositiveValueOnly]
     private float timeBetweenSteps = 1.0f;
@@ -21,6 +24,7 @@ public class Treadmill : MonoBehaviour, IInteractable
     private float insulinSensitivityIncrease = 3f;
     
     private bool isOnTreadmill = false;
+    private float timeStarted = 0.0f;
     private float lastStepTime = 0.0f;
     
     private void Update()
@@ -39,6 +43,8 @@ public class Treadmill : MonoBehaviour, IInteractable
             return;
         }
         
+        timerText.text = TimeOfDay.FromSeconds((int)(TimeManager.time - timeStarted)).ToString()[..^2];
+        
         //TODO prompt the user to press a key to take a step
         //TODO slow down simulation time for BG simulator
         //TODO tie exercise insulin sensitivity to BPM increased from treadmill via PlayerVitals new script?
@@ -52,6 +58,7 @@ public class Treadmill : MonoBehaviour, IInteractable
     public void Interact()
     {
         isOnTreadmill = true;
+        timeStarted = TimeManager.time;
         
         player.position = playerWalkingPoint.position;
         player.rotation = playerWalkingPoint.rotation;
