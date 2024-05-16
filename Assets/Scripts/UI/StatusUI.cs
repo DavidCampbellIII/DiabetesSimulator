a81d24a44f37fa61cdea9d7cd80740fc8452de42
David Campbell III <lapidistcubed@gmail.com>
2024-05-16 01:50:31 -0500

    Added computer and started StatusUI

---
+++ Assets/Scripts/UI/StatusUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using MyBox;
+using TMPro;
+using UnityEngine;
+
+public class StatusUI : MonoBehaviour
+{
+    [SerializeField, MustBeAssigned]
+    private TextMeshProUGUI interactText;
+    [SerializeField, MustBeAssigned]
+    private TextMeshProUGUI statusText;
+    
+    //TODO
+}
